b026520cbb689d87abc222964d0efbd0842d4ca8
Yash <93475253+kekda-py@users.noreply.github.com>
2022-06-01 16:59:24 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,16 +10,15 @@ const Home: NextPage = () => {
 			</Head>
 			<h1>Posts</h1>
 			<ul>
-
+				<LinkPost
+					name="Write and deploy blogs in seconds with comments support"
+					url="write-blogs-seconds"
+					dsc="A few days ago I wrote my first blog and wanted to make a website for it. So I made one using Next.js and MDX. While making it I got an idea..."
+				/>
 				<LinkPost
 					name="How I made a completely Anonymous chat app with go and next.js"
 					url="buga-chat-first"
 					dsc="So I was learning `go` the other day. And was amazed by its channels. So decided to make something in it. Actually I thought..."
-				/>
-				<LinkPost
-					name="Write and deploy blogs in seconds with comments support"
-					url="write-blogs-seconds"
-					dsc="A few days ago I wrote my first blog and wanted to make website for it. So I made one using Next.js and MDX. While making it I got an idea..."
 				/>
 
 			</ul>
